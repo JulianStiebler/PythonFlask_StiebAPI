4d3f94ca0c2b11bcc54e63a63eea7243a1724e4f
JulianStiebler <68881884+JulianStiebler@users.noreply.github.com>
2023-04-22 22:58:01 +0200

    merged designs

--- page/flask_blog.py
+++ page/flask_blog.py
@@ -17,8 +17,12 @@ posts = [
 ]
 
 @app.route("/") # -> MAIN ROUTE
-def routes_main():
+def routes_home():
     return render_template('pages/home.html', posts=posts)
+
+@app.route("/dashboard/") # 
+def routes_dashboard():
+    return render_template('pages/dashboard.html', posts=posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
