b0cbd755718267520b29aa1e5fc36704bc331808
JulianStiebler <68881884+JulianStiebler@users.noreply.github.com>
2023-04-25 22:05:41 +0200

    basic sqlalchemy logic

--- page/flask_blog.py
+++ page/flask_blog.py
@@ -1,30 +1,41 @@
+from datetime import datetime
 from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import forms_register, forms_login
-app = Flask(__name__)
 
+app = Flask(__name__)
 # REMEMBER TO MAKE AN ENVIRONMENT VARIABLE AT PRODUCTION!
 app.config['SECRET_KEY'] = '1aa37a53492185cad4fdadd2793bef9b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-posts = [
-    {
-        'author': 'Max Mustermann',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Yesterday'
-    },
-    {
-        'author': 'Max Mustermann',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Today'
-    }
-]
-title = 'Main'
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 @app.route("/")
 @app.route("/home/")
 def routes_home():
-    return render_template('pages/home.html', title='Home', posts=posts)
+    return render_template('pages/home.html', title='Home')
 
 @app.route("/register/", methods=['GET', 'POST'])
 def routes_register():
