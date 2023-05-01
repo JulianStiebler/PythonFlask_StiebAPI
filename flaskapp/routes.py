7f75c15ac7667637c607605e6bd443829aa7e22a
JulianStiebler <68881884+JulianStiebler@users.noreply.github.com>
2023-05-01 23:02:46 +0200

    up

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -22,9 +22,10 @@ posts = [
 @app.route("/")
 @app.route("/home/")
 def routes_home():
-    return render_template('pages/home.html', title='Home', posts=posts)
+    return render_template('pages/home/home.html', 
+                           title='Home', posts=posts)
 
-@app.route("/register/", methods=['GET', 'POST'])
+@app.route("/user/register/", methods=['GET', 'POST'])
 def routes_register():
     if current_user.is_authenticated:
         return redirect(url_for('routes_dashboard'))
@@ -39,9 +40,10 @@ def routes_register():
         db.session.commit()
         flash(f'Account created for {form.email.data}!', 'success')
         return redirect(url_for('routes_login'))
-    return render_template('pages/register.html', title='Register', form=form)
+    return render_template('pages/user/register.html', 
+                           title='Register', form=form)
 
-@app.route("/login/", methods=['GET', 'POST'])
+@app.route("/user/login/", methods=['GET', 'POST'])
 def routes_login():
     if current_user.is_authenticated:
         return redirect(url_for('routes_dashboard'))
@@ -56,20 +58,23 @@ def routes_login():
                 if next_page else redirect(url_for('routes_dashboard'))
         else:
             flash('Wrong login data! Please check your user input.', 'danger')
-    return render_template('pages/login.html', title='Login', form=form)
+    return render_template('pages/user/login.html', 
+                           title='Login', form=form)
 
-@app.route("/logout/")
+@app.route("/user/logout/")
 def routes_logout():
     logout_user()
     return redirect(url_for('routes_home'))
 
 
-@app.route("/account/")
+@app.route("/user/account")
 @login_required
 def routes_account():
-    return render_template('pages/user/account.html', title='Account')
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('pages/user/account.html', 
+                           title='Account', image_file=image_file)
 
 @app.route("/dashboard/") 
 @login_required
 def routes_dashboard():
-    return render_template('pages/dashboard.html', title='Dashboard')
+    return render_template('pages/dashboard/dashboard.html', title='Dashboard', )
