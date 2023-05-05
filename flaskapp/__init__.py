0a6629a495bc5b86ee4219a889d15025e17754b6
JulianStiebler <68881884+JulianStiebler@users.noreply.github.com>
2023-05-05 21:54:48 +0200

    Added docstring to __init__.py

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -4,6 +4,10 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_minify import Minify
 
+"""
+Initiliaze the app and append some config, pass it to DB and start Flask-Migrate,
+add BCrypt and LoginManager and then add Limiter and minify.
+"""
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1aa37a53492185cad4fdadd2793bef9b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -16,9 +20,9 @@ Minify(app=app, html=True, js=True, cssless=True, static=True)
 
 
 """
-    Since we need to avoid circular imports we are forced to import
-    routes after we created App, and we are not going to do anything
-    more here, so we need to ignore two Ruff-errors.
-    (Import not at Header-Level & import routes not used)
+Since we need to avoid circular imports we are forced to import
+routes after we created App, and we are not going to do anything
+more here, so we need to ignore two Ruff-errors.
+(Import not at Header-Level & import routes not used)
 """
 from flaskapp import routes  # noqa: E402, F401
