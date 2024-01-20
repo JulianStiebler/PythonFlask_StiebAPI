22642ff209edaa5c9b655297e989eb1b1a430e1f
Julian Stiebler <68881884+JulianStiebler@users.noreply.github.com>
2024-01-20 23:03:30 +0100

    fixes #15

--- run.py
+++ run.py
@@ -3,5 +3,6 @@ from flaskapp import app
 if __name__ == "__main__":
     # Debug in dev-environment only!
     # app.run(debug=True, host='192.168.0.50', port=5000)
+    
     app.run(debug=False, host='192.168.0.50', port=5000)
 
